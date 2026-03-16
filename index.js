fc061fa0cf7b171daccded3f08d8e75c2a34321f
unknown <aa133092@seeu.edu.mk>
2026-03-16 14:41:24 +0100

    Inicializimi i Git

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("Hello git!");
+
+//git init - inicializim i repository
+//git status - statusi i fajllave, pra i branches (branch kryesor quhet master)
+//git add - e shtojme ne repository fajllat. me git add ., kjo pika i selekton krejt
+//git rm - e heq

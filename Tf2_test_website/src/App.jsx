08d5471456428184d72e522fd7de756387fcd917
AritonAjdini <aa33092@seeu.edu.mk>
2026-04-18 10:49:20 +0200

    I always come bacje

--- Tf2_test_website/src/App.jsx
+++ Tf2_test_website/src/App.jsx
@@ -1,21 +1,11 @@
 import './App.css'
 import { Tabbleadder } from './Elements'
+
 export default function App() {
     return(
     <div className="gridbox">
     <img src="src/assets/TF2_Icon.svg" className='floating'/>
-        <div>
-            <div id="inputs">
-                <h1>TF2 User Database</h1>
-                <input name="username" type="text" placeholder="Name of User" className="inpcss" id="first" const />
-                <input name="Steamid" type="number" placeholder="SteamId" className="inpcss" id="second"/>
-            </div>
-            <div id="buttons">
-                <button className="btn" id="insert" data-tooltip="Insert the Username and SteamId">Insert</button>
-                <button className="btn" id="delete" data-tooltip="Delete the elements of the table">Delete</button>
-            </div>
-        </div>
-  
+        <Tabbleadder/>
        <table>
             <thead>
               <tr>
@@ -24,7 +14,7 @@ export default function App() {
               </tr>
            </thead>
            <tbody>
-               <Tabbleadder/>
+               
            </tbody>
         </table>
         </div>
--- Tf2_test_website/src/Elements.jsx
+++ Tf2_test_website/src/Elements.jsx
@@ -1,17 +1,34 @@
+import { useState } from 'react'
+
 export const Tabbleadder = () => {
+    const [firstName, setFirstName] = useState('');
+    // const valid = frst && scnd;
 
-    const valid = frst && scnd;
-
-    return valid ? (
+    
+    return (
+        <>
+             <div>
+            <div id="inputs">
+                <h1>TF2 User Database</h1>
+                <input name="username" type="text" placeholder="Name of User" className="inpcss" id="first" const />
+                <input name="Steamid" type="number" placeholder="SteamId" className="inpcss" id="second"/>
+            </div>
+            <div id="buttons">
+                <button className="btn" id="insert" data-tooltip="Insert the Username and SteamId">Insert</button>
+                <button className="btn" id="delete" data-tooltip="Delete the elements of the table">Delete</button>
+            </div>
+        </div>  
+   
         <tr>
-            <td>{frst}</td>
-            <td>{scnd}</td>
+            <td>hello</td>
+            <td>hello</td>
         </tr>
-    ) : (
+    
         <tr>
             <td colSpan="2" style={{ color: "red", textAlign: "center" }}>
                 Please insert the data correctly!
             </td>
         </tr>
+     </>
     );
 }; 

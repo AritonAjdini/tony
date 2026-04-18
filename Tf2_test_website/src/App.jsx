13c1d1b77fef6afecbc66ac724f262018c4bd283
AritonAjdini <aa33092@seeu.edu.mk>
2026-04-18 18:38:15 +0200

    New features: RegEx control!

--- Tf2_test_website/src/App.jsx
+++ Tf2_test_website/src/App.jsx
@@ -1,23 +1,35 @@
 import './App.css'
-import { Tabbleadder } from './Elements'
+import { Tabbleinput } from './Elements'
 
+import { useState } from 'react'
 export default function App() {
-    return(
+    const [firstName, setFirstName] = useState('');
+    return( 
     <div className="gridbox">
-    <img src="src/assets/TF2_Icon.svg" className='floating'/>
-        <Tabbleadder/>
+      <img src="src/assets/TF2_Icon.svg" className='floating'/>
+          <div>
+            <div id="inputs">
+                <h1>TF2 User Database</h1>
+                <Tabbleinput type="text"/>
+                <Tabbleinput type="number" />
+            </div>
+            <div id="buttons">
+                <button className="btn" id="insert" data-tooltip="Insert the Username and SteamId">Insert</button>
+                <button className="btn" id="delete" data-tooltip="Delete the elements of the table">Delete</button>
+            </div>
+         </div>  
        <table>
             <thead>
               <tr>
                    <th id="firsthd">Name</th>
                    <th id="scndhd">SteamID</th>
               </tr>
-           </thead>
-           <tbody>
-               
-           </tbody>
+            </thead>
+            <tbody>
+                    
+            </tbody>
         </table>
-        </div>
+    </div>   
     )
         
 }
--- Tf2_test_website/src/Elements.jsx
+++ Tf2_test_website/src/Elements.jsx
@@ -1,34 +1,48 @@
 import { useState } from 'react'
 
-export const Tabbleadder = () => {
-    const [firstName, setFirstName] = useState('');
-    // const valid = frst && scnd;
+const RegExValidInputs = {
+        username: /^[a-z0-9_-]{3,16}$/,
+        steamIdentification: /^[0-9]{17}$/,
+}
+{/*Only Usernames with atleast 3 characters and upto 16 are allowed*/}
+{/*SteamID accepts only number strings of 17 numbers*/}
 
-    
+export const Tabbleinput = ({type}) => {
+    const [InputValue, setInputValue] = useState('');
+    const [Error, setError] = useState('');
+
+    let IsUsrnm = RegExValidInputs.username
+    let IsStmD = RegExValidInputs.steamIdentification
+
+    const Changeinput=(ev)=>{
+        const val = ev.target.value;
+        setInputValue(val);
+        const IsUsername=IsUsrnm.test(val);
+        const IsSteamID=IsStmD.test(val);
+        if(type === "text"){
+           (IsUsername)? setError("") : setError("Only Usernames with atleast 3 characters and upto 16 are allowed!")
+        }
+        if(type === "number"){
+           (IsSteamID)? setError("") : setError("SteamID accepts only number strings of 17 numbers!")
+        }
+    }
+
     return (
-        <>
-             <div>
-            <div id="inputs">
-                <h1>TF2 User Database</h1>
-                <input name="username" type="text" placeholder="Name of User" className="inpcss" id="first" const />
-                <input name="Steamid" type="number" placeholder="SteamId" className="inpcss" id="second"/>
-            </div>
-            <div id="buttons">
-                <button className="btn" id="insert" data-tooltip="Insert the Username and SteamId">Insert</button>
-                <button className="btn" id="delete" data-tooltip="Delete the elements of the table">Delete</button>
-            </div>
-        </div>  
-   
-        <tr>
-            <td>hello</td>
-            <td>hello</td>
-        </tr>
-    
-        <tr>
-            <td colSpan="2" style={{ color: "red", textAlign: "center" }}>
-                Please insert the data correctly!
-            </td>
-        </tr>
-     </>
+        <div style={{padding: '2px'}}>
+            <input
+             value={InputValue}
+             onChange={Changeinput}
+             style={{
+                border: Error ? '2px solid #ff0000' : '1px solid rgb(46, 250, 0)',
+             }}
+             placeholder={
+                (type === "text")? "Username" : "SteamID"
+             }
+             className="inpcss"
+             />
+        
+             {Error && <p style={{ color: 'red', fontSize: '15px', position: "center" }}>{Error}</p>}
+             {!Error && InputValue && <p style={{ color: 'green', fontSize: '15px', position: "center" }}>Looking good!</p>}
+        </div>
     );
 }; 
